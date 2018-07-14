8bd23c80e826b550a9f9ed147491c70c4ce33675
splittydev <splittydev@protonmail.com>
2018-07-14 12:56:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# rl
+> Extract line ranges from piped input.
+
+## Why
+I frequently find myself googling 'linux get specific line from file'.
+
+Here's what usually pops up[<sup>*1</sup>](#footnotes):
+- `cat myfile | sed -n '10,20p;21q'`
+- `cat myfile | awk 'NR>=10&&NR<=20'`
+
+I'm looking this up more often than I wanna admit.   
+So I wrote a simple solution for extracting line ranges.
+
+Of course sed and awk are way more powerful, but if you just wanna extract some lines,   
+and - like me - just can't remember those commands, rl could be a good alternative for you.
+
+## Syntax
+rl has a very simple range-based syntax.   
+It is parsed by the following regexp: `/(\^|\d+)?(?:-(\d+|\$))?/g`
+
+A range is basically `start`-`end`.   
+The following special characters are supported:
+- `^` refers to first line
+- `$` refers to last line
+
+Multiple ranges can be specified, separated by comma (`,`) characters.
+
+## Behavior
+Lines being matched by multiple ranges are only output once.
+
+Ranges covering the full line range will be treated as pass-through,   
+which means they will simply output their input as-is: (`-`, `^-$`).
+
+## Examples
+Due to the special markers (`^` and `$`) there will be some cases   
+where multiple expressions yield the same result.
+
+In the following examples, all possible combinations will be listed.
+
+Get the first line of a file:
+> `$ cat myfile | rl ^`   
+> `$ cat myfile | rl 1`
+
+Get the first 10 lines of a file:
+> `$ cat myfile | rl 1-10`   
+> `$ cat myfile | rl ^-10`
+
+Get lines 5 to 10 and lines 25 to 30:
+> `$ cat myfile | rl 5-10,25-30`
+
+Get lines 1 to 10 and lines 50 to end:
+> `$ cat myfile | rl 1-10,50-$`   
+> `$ cat myfile | rl ^-10,50-$`-
+
+Get all lines of a file <sub>(Seems pretty useless to me)</sub>:
+> `$ cat myfile | rl -`   
+> `$ cat myfile | rl ^-$`
+
+<br><hr>
+<a name="footnotes"></a>
+<sub>
+Forgive me the use of cat for reading a file.<br>
+Since rl was written specifically for piped input, using cat seemed more natural.
+</sub>
---
+++ rl.js
@@ -0,0 +1,132 @@
+#!/usr/bin/env node
+
+// Constants
+const CHUNK_SIZE = 512;
+const REGEX_EXTRACT_RANGES = /(\^|\d+)?(?:-(\d+|\$))?/g;
+
+// Grab streams
+const stdin = process.stdin;
+const stdout = process.stdout;
+
+class Range {
+    constructor([a, b]) {
+        this.a = a;
+        this.b = b;
+        this.passthrough = (false
+            || (this.a.isUndefined && this.b.isUndefined)
+            || (this.a.fromFirst && this.b.toLast)
+        );
+    }
+
+    matches(lineNum) {
+        // Pass-through
+        if (this.passthrough) {
+            return true;
+        }
+        if (this.a.isNum) {
+            // Just one line
+            if (this.b.isUndefined) {
+                return this.a.num === lineNum;
+            }
+            // Both markers are numbers
+            if (this.b.isNum) {
+                return lineNum >= this.a.num && lineNum <= this.b.num;
+            }
+            // End marker is last line
+            if (this.b.toLast) {
+                return lineNum >= this.a.num;
+            }
+        } else if (this.a.fromFirst) {
+            // From start to line
+            if (this.b.isNum) {
+                return lineNum <= this.b.num;
+            }
+        }
+        console.log('ERROR! Unhandled case: ');
+        console.log(this.a);
+        console.log(this.b);
+        process.exit(1);
+    }
+}
+
+class RangeMarker {
+    constructor(str) {
+        this.fromFirst = str === '^';
+        this.toLast = str === '$';
+        this.num = parseInt(str);
+        this.isNum = !isNaN(this.num);
+        this.isUndefined = str === undefined;
+    }
+}
+
+class LineProcessor {
+    constructor(ranges) {
+        this.accum = '';
+        this.lineCount = 0;
+        this.ranges = ranges;
+    }
+
+    processLines(lines) {
+        while (lines.length > 1) {
+            this.processLine(lines.shift());
+        }
+        return lines.shift(); // rest
+    }
+
+    processLine(line) {
+        this.lineCount++;
+        for (const range of this.ranges) {
+            if (range.matches(this.lineCount)) {
+                stdout.write(line);
+                stdout.write('\n');
+            }
+        }
+    }
+
+    processLastLine() {
+        this.processLine(this.accum);
+    }
+
+    processChunk(chunk) {
+        let data = this.accum + chunk.toString('utf-8');
+        let dataLines = data.split('\n');
+        this.accum = this.processLines(dataLines);
+    }
+}
+
+function main() {
+    // Parse query parts
+    const query = process.argv[2];
+    if (query === undefined) return;
+    const queryParts = query.split(',');
+
+    // Extract ranges
+    const ranges = queryParts
+        .map(p => REGEX_EXTRACT_RANGES.exec(p))
+        .map(([, start, end]) => [
+            new RangeMarker(start),
+            new RangeMarker(end)
+        ])
+        .map(range => new Range(range));
+
+    // Create line processor
+    const processor = new LineProcessor(ranges);
+
+    stdin.on('end', () => {
+        // Process the last line
+        processor.processLastLine();
+    });
+
+    stdin.on('readable', () => {
+        let chunk;
+
+        // Read chunks of input
+        while ((chunk = stdin.read(CHUNK_SIZE)) !== null) {
+
+            // Process the latest input chunk
+            processor.processChunk(chunk);
+        }
+    });
+}
+
+main();
